26540b069987faf73ea7148ea8e7012bec195141
Theophilus Confidence <spanzytheo@gmail.com>
2026-02-13 11:03:40 +0100

    modifications on newsletter api

--- src/newsletter/dto/update-newsletter.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateNewsletterDto } from './create-newsletter.dto';
-
-export class UpdateNewsletterDto extends PartialType(CreateNewsletterDto) {}
